f91c324d51abc37749cf03368f572aabb66181d2
KENNEY SHUN <150319058+shunkenney@users.noreply.github.com>
2025-11-10 22:08:49 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-# Clone git repositories that you will edit source.
-git clone <url/of/lib3/git_repo> <my_proj>/submodules/<lib3>
-rm -rf <my_proj>/submodules/<lib3>/.git
-
-# Setup virtual env
-uv sync
